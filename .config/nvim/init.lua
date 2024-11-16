0c67292b20d36537dde8d07ae07bdbd580b7404d
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-11-16 21:01:43 +0600

    Instructions On Init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,9 @@ require("config.autocmds")
 
 vim.g.base46_cache = vim.fn.stdpath('data') .. '/base46_cache/'
 
+-- At First Time, Uncomment this line, Then Open Neovim, Then Comment this line again Or Delete it.
+-- require('base46').load_all_highlights()
+
 dofile(vim.g.base46_cache .. "statusline")
 dofile(vim.g.base46_cache .. "devicons")
 dofile(vim.g.base46_cache .. "telescope")
