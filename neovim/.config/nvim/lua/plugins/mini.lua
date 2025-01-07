426f30707b433d91379fa0cebc9bea004b4c08a0
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-01-07 17:15:24 +0600

    fix indentscope, now doesn't mess up in disabled filetypes

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -7,15 +7,38 @@ return {
   },
   {
     "echasnovski/mini.indentscope",
-    config = function()
+    event = "VeryLazy",
+    opts = {
+      options = { try_as_border = true },
+    },
+    init = function()
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = "*",
+        pattern = {
+          "Trouble",
+          "alpha",
+          "dashboard",
+          "fzf",
+          "help",
+          "lazy",
+          "mason",
+          "neo-tree",
+          "notify",
+          "snacks_dashboard",
+          "snacks_notif",
+          "snacks_terminal",
+          "snacks_win",
+          "toggleterm",
+          "trouble",
+        },
         callback = function()
-          local filetype = vim.bo.filetype
-          local disabled_filetypes = { "snacks_dashboard" }
-          if not vim.tbl_contains(disabled_filetypes, filetype) then
-            require("mini.indentscope").setup()
-          end
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "SnacksDashboardOpened",
+        callback = function(data)
+          vim.b[data.buf].miniindentscope_disable = true
         end,
       })
     end,
