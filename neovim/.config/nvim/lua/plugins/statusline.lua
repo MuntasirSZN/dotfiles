12d13086b9d7d0294c0ed746390953f6abe2e6d1
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-02-01 17:19:52 +0600

    i want the middle part transparent, wrote some lua for it

--- neovim/.config/nvim/lua/plugins/statusline.lua
+++ neovim/.config/nvim/lua/plugins/statusline.lua
@@ -5,10 +5,59 @@ return {
 		"AndreM222/copilot-lualine",
 	},
 	config = function()
+		local catppuccin_flavours = {
+			["catppuccin-mocha"] = "mocha",
+			["catppuccin-macchiato"] = "macchiato",
+			["catppuccin-frappe"] = "frappe",
+			["catppuccin-latte"] = "latte",
+		}
+
+		local catppuccin_flavor_set = {
+			mocha = true,
+			macchiato = true,
+			frappe = true,
+			latte = true,
+		}
+
+		local function get_flavor()
+			local theme = require("themery").getCurrentTheme()
+			if not theme then
+				return
+			end
+			return catppuccin_flavours[theme.name] or theme.name
+		end
+
+		local function get_flavoured_theme()
+			local theme_name = get_flavor()
+			if not theme_name then
+				return
+			end
+
+			if catppuccin_flavor_set[theme_name] then
+				local lualine_theme = require("catppuccin.utils.lualine")(theme_name)
+
+				-- Clear background for specific sections
+				local clear_bg_sections = {
+					normal = { c = true },
+					inactive = { a = true, b = true, c = true },
+				}
+
+				for section, parts in pairs(clear_bg_sections) do
+					for part in pairs(parts) do
+						lualine_theme[section][part].bg = "NONE"
+					end
+				end
+
+				return lualine_theme
+			end
+
+			return theme_name
+		end
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "catppuccin",
+				theme = get_flavoured_theme(),
 				globalstatus = vim.o.laststatus == 3,
 				disabled_filetypes = {
 					winbar = {
