21d151adf78cdd962b68b6bdff412fe1f4d4798a
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-02-12 18:00:20 +0600

    use cliphist

--- hypr/.config/hypr/scripts/ClipManager.sh
+++ hypr/.config/hypr/scripts/ClipManager.sh
@@ -1,8 +1,44 @@
-#!/usr/bin/bash
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Clipboard Manager. This script uses cliphist, rofi, and wl-copy.
 
-rofi_clipboard() {
-  rofi -i -dmenu -config "~/.config/rofi/config-clipboard.rasi"
-}
-export -f rofi_clipboard
+# Actions:
+# CTRL Del to delete an entry
+# ALT Del to wipe clipboard contents
 
-env CM_LAUNCHER=rofi_clipboard CM_OUTPUT_CLIP=1 clipmenu | pbcopy
+# Check if rofi is already running
+if pidof rofi >/dev/null; then
+  pkill rofi
+fi
+
+while true; do
+  result=$(
+    rofi -i -dmenu \
+      -kb-custom-1 "Control-Delete" \
+      -kb-custom-2 "Alt-Delete" \
+      -config ~/.config/rofi/config-clipboard.rasi < <(cliphist list)
+  )
+
+  case "$?" in
+  1)
+    exit
+    ;;
+  0)
+    case "$result" in
+    "")
+      continue
+      ;;
+    *)
+      cliphist decode <<<"$result" | wl-copy
+      exit
+      ;;
+    esac
+    ;;
+  10)
+    cliphist delete <<<"$result"
+    ;;
+  11)
+    cliphist wipe
+    ;;
+  esac
+done
