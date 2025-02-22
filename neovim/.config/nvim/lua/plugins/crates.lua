efba2997d13a39bbafe40d90abb9b4125ccf4fbd
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-02-22 19:59:05 +0600

    yazi plugin updates, don't use enabled in crates, remove old keymaps

--- neovim/.config/nvim/lua/plugins/crates.lua
+++ neovim/.config/nvim/lua/plugins/crates.lua
@@ -1,11 +1,6 @@
 return {
 	"saecki/crates.nvim",
-	enabled = function()
-		if vim.fn.expand("%:t") == "Cargo.toml" then
-			return true
-		end
-		return false
-	end,
+	event = "BufRead Cargo.toml",
 	tag = "stable",
 	opts = {
 		completion = {
@@ -23,32 +18,44 @@ return {
 	init = function()
 		local wk = require("which-key.config")
 		local crates = require("crates")
-		wk.add({
-			{ "<leader>cc", group = "Crates" },
-			{ "<leader>cct", crates.toggle, desc = "Toggle" },
-			{ "<leader>ccr", crates.reload, desc = "Reload" },
+		if vim.fn.expand("%:t") == "Cargo.toml" then
+			wk.add({
+				{ "<leader>cc", group = "Crates" },
+				{ "<leader>cct", crates.toggle, desc = "Toggle" },
+				{ "<leader>ccr", crates.reload, desc = "Reload" },
 
-			{ "<leader>ccv", crates.show_versions_popup, desc = "Versions popup" },
-			{ "<leader>ccf", crates.show_features_popup, desc = "Features popup" },
-			{ "<leader>ccd", crates.show_dependencies_popup, desc = "Dependencies popup" },
+				{ "<leader>ccv", crates.show_versions_popup, desc = "Versions popup" },
+				{ "<leader>ccf", crates.show_features_popup, desc = "Features popup" },
+				{ "<leader>ccd", crates.show_dependencies_popup, desc = "Dependencies popup" },
 
-			{ "<leader>ccu", crates.update_crate, desc = "Update crate" },
-			{ "<leader>ccu", crates.update_crates, desc = "Update selected crates", mode = "v" },
-			{ "<leader>cca", crates.update_all_crates, desc = "Update all crates" },
+				{ "<leader>ccu", crates.update_crate, desc = "Update crate" },
+				{
+					"<leader>ccu",
+					crates.update_crates,
+					desc = "Update selected crates",
+					mode = "v",
+				},
+				{ "<leader>cca", crates.update_all_crates, desc = "Update all crates" },
 
-			{ "<leader>ccU", crates.upgrade_crate, desc = "Upgrade crate" },
-			{ "<leader>ccU", crates.upgrade_crates, desc = "Upgrade selected crates", mode = "v" },
-			{ "<leader>ccA", crates.upgrade_all_crates, desc = "Upgrade all crates" },
+				{ "<leader>ccU", crates.upgrade_crate, desc = "Upgrade crate" },
+				{
+					"<leader>ccU",
+					crates.upgrade_crates,
+					desc = "Upgrade selected crates",
+					mode = "v",
+				},
+				{ "<leader>ccA", crates.upgrade_all_crates, desc = "Upgrade all crates" },
 
-			{ "<leader>ccx", crates.expand_plain_crate_to_inline_table, desc = "Expand to inline table" },
-			{ "<leader>ccX", crates.extract_crate_into_table, desc = "Extract into table" },
-			{ "<leader>ccg", crates.use_git_source, desc = "Use git source" },
+				{ "<leader>ccx", crates.expand_plain_crate_to_inline_table, desc = "Expand to inline table" },
+				{ "<leader>ccX", crates.extract_crate_into_table, desc = "Extract into table" },
+				{ "<leader>ccg", crates.use_git_source, desc = "Use git source" },
 
-			{ "<leader>ccH", crates.open_homepage, desc = "Open homepage" },
-			{ "<leader>ccR", crates.open_repository, desc = "Open repository" },
-			{ "<leader>ccD", crates.open_documentation, desc = "Open documentation" },
-			{ "<leader>ccC", crates.open_crates_io, desc = "Open crates.io" },
-			{ "<leader>ccL", crates.open_lib_rs, desc = "Open lib.rs" },
-		})
+				{ "<leader>ccH", crates.open_homepage, desc = "Open homepage" },
+				{ "<leader>ccR", crates.open_repository, desc = "Open repository" },
+				{ "<leader>ccD", crates.open_documentation, desc = "Open documentation" },
+				{ "<leader>ccC", crates.open_crates_io, desc = "Open crates.io" },
+				{ "<leader>ccL", crates.open_lib_rs, desc = "Open lib.rs" },
+			})
+		end
 	end,
 }
