0f53658a2bd67845a5ab42ec0fe052af7929c57c
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-03-17 19:32:29 +0600

    more edits

--- neovim/.config/nvim/lua/plugins/colorizer.lua
+++ neovim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,11 +1,52 @@
 return {
-  "catgoose/nvim-colorizer.lua",
-  event = "BufReadPre",
-  lazy = true,
-  keys = {
-    { "<leader>ce", "<cmd>ColorizerToggle<CR>", desc = "Toggle colorizer" },
-  },
-  opts = {
-    filetypes = {},
-  },
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = {
+		filetypes = {
+			"*",
+			"!Trouble",
+			"!alpha",
+			"!dashboard",
+			"!fzf",
+			"!help",
+			"!lazy",
+			"!mason",
+			"!neo-tree",
+			"!notify",
+			"!snacks_dashboard",
+			"!snacks_notif",
+			"!snacks_terminal",
+			"!snacks_win",
+			"!toggleterm",
+			"!trouble",
+		},
+		user_default_options = {
+			virtualtext = "󱓻",
+			mode = "virtualtext",
+			RRGGBBAA = true,
+			AARRGGBB = true,
+			css = true,
+			css_fn = true,
+			virtualtext_inline = "before",
+			tailwind = true,
+			tailwind_opts = {
+				update_names = true,
+			},
+			sass = { enable = true, parsers = { "css" } },
+		},
+	},
+	init = function()
+		local c = require("colorizer")
+		vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+			callback = function(args)
+				if args.event == "InsertEnter" then
+					c.detach_from_buffer()
+				else
+					if not c.is_buffer_attached() then
+						c.attach_to_buffer()
+					end
+				end
+			end,
+		})
+	end,
 }
---
+++ neovim/.config/nvim/lua/plugins/modes.lua
@@ -0,0 +1,7 @@
+return {
+	"mvllow/modes.nvim",
+	lazy = false,
+	config = function()
+		require("modes").setup()
+	end,
+}
