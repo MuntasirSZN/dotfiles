dca981a175e04b1f22a9983214548d5189adae53
MuntasirSZN <muntasir.joypurhat@gmail.com>
2026-07-06 19:33:37 +0600

    up

--- nixos/.config/nixos/modules/nix.nix
+++ nixos/.config/nixos/modules/nix.nix
@@ -2,16 +2,19 @@
 _:
 
 {
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    trusted-users = [
-      "root"
-      "muntasir"
-    ];
-    accept-flake-config = true;
+  nix = {
+    optimise.automatic = true;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "muntasir"
+      ];
+      accept-flake-config = true;
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
