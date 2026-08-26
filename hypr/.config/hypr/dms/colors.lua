49bff34a3495f00125d99c358c3397aaeb2c6176
Muntasir Mahmud <muntasir.joypurhat@gmail.com>
2026-08-26 15:01:01 +0600

    stuff
    Signed-off-by: Muntasir Mahmud <muntasir.joypurhat@gmail.com>

--- hypr/.config/hypr/dms/colors.lua
+++ hypr/.config/hypr/dms/colors.lua
@@ -4,23 +4,23 @@
 hl.config({
 	general = {
 		col = {
-			active_border = "rgb(a5c8ff)",
-			inactive_border = "rgb(8d9199)",
+			active_border = "rgb(82d3e1)",
+			inactive_border = "rgb(899294)",
 		},
 	},
 	group = {
 		col = {
-			border_active = "rgb(a5c8ff)",
-			border_inactive = "rgb(8d9199)",
+			border_active = "rgb(82d3e1)",
+			border_inactive = "rgb(899294)",
 			border_locked_active = "rgb(ffb4ab)",
-			border_locked_inactive = "rgb(8d9199)",
+			border_locked_inactive = "rgb(899294)",
 		},
 		groupbar = {
 			col = {
-				active = "rgb(a5c8ff)",
-				inactive = "rgb(8d9199)",
+				active = "rgb(82d3e1)",
+				inactive = "rgb(899294)",
 				locked_active = "rgb(ffb4ab)",
-				locked_inactive = "rgb(8d9199)",
+				locked_inactive = "rgb(899294)",
 			},
 		},
 	},
